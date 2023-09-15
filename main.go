dfef8759a6a4bd799e6e8e8e9eda0f5cf27b73d1
mhargain <martinhargain2@gmail.com>
2023-09-15 13:01:16 -0300

    config-snippet

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-    "os"
-    "net/http"
+	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    podName := os.Getenv("POD_NAME")
-    w.Write([]byte("POD_NAME: " + podName))
+	podName := os.Getenv("POD_NAME")
+	url := r.URL.String()
+	host := r.Host
+	path := r.URL.Path
+	scheme := r.URL.Scheme
+
+	w.Write([]byte("POD_NAME: " + podName + "\nURL: " + url + "\nHOST: " + host + "\nPATH: " + path + "\nSCHEME: " + scheme))
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
